872b898b0ad55635a1ad32536bff8f89fc0cdbab
Michael Osibote Omoboye <P2532539@my365.dmu.ac.uk>
2021-03-17 06:21:33 +0000

    17/03/21 completed

---
+++ ClassLibrary/clsStaff.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStaff
+    {
+        private DateTime mEmploymentDate;
+        private string mStaffAddress;
+        private string mStaffName;
+        private int mStaffNO;
+        private Boolean mStaffGender;
+        public string StaffName
+        {
+            get
+
+            {
+                return mStaffName;
+            }
+            set
+            {
+                mStaffName = value;
+            }
+        }
+        public string staffAddress
+        {
+            get
+            {
+                return mStaffAddress;
+
+            }
+            set
+            {
+                mStaffAddress = value;
+            }
+        }
+        public Int32 StaffNO
+        {
+            get
+            {
+                return mStaffNO;
+            }
+            set
+            {
+                mStaffNO = value;
+            }
+        }
+        public DateTime EmploymentDate
+        {
+            get
+            {
+                return mEmploymentDate;
+            }
+            set
+            {
+                mEmploymentDate = value;
+            }
+            
+        }
+
+        public string Valid(string staffName, string staffAddress, string employmentDate, string staffNo)
+        {
+            string Error = "";
+            DateTime DateTemp;
+            int IntTemp;
+
+
+            if (staffName.Length < 1)
+            {
+                Error = Error + "Staff Name cannot be left blank";
+            }
+            if (mStaffName.Length > 50)
+            {
+                Error = Error + " Name is greater than 50 characters";
+            }
+            if (staffAddress.Length < 1)
+            {
+                Error = Error + "invalid StaffAddress";
+            }
+            if (staffAddress.Length > 50)
+            {
+                Error = Error + "the The Staff Address can not be greater than 50 characters"
+            }
+
+            try
+            {
+                IntTemp = Convert.ToInt32(staffNo);
+                if (IntTemp < 0)
+                {
+                    Error = Error + "Staff Nuumber can not ne lower than ZERO!";
+                }
+                if (IntTemp > Int32.MaxValue)
+                {
+                    Error = Error + "Max integer Value has been Exceeded";
+                }
+            }
+            catch
+            {
+                Error = Error + "not a valid Staff Number";
+            }
+            try
+            {
+                DateTemp = Convert.ToDateTime(employmentDate);
+                if (DateTemp < DateTime.Now.Date.AddYears(-50))
+                {
+                    Error = Error + "no staff member was added 50 years ago ";
+                }
+                if (DateTemp > DateTime.Now.Date)
+                {
+                    Error = Error + " Staff members cannot be addd in the future";
+                }
+            }
+            catch
+            {
+                Error = Error + "invalid Date";
+            }
+            return Error;
+        }
+    }
+}
+       
+    
---
+++ Testing3/tstStaff.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStaff
+    {
+        //good method
+        private string StaffName = "Staff Name";
+        private string StaffAddress = "Staff Address";
+        private string EmploymentDate = DateTime.Now.Date.ToString();
+        private string StaffNO = "3";
+
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsStaff aStaff = new clsStaff();
+            Assert.IsNotNull(aStaff);
+        }
+        [TestMethod]
+        public void StaffNameOK()
+        {
+            clsStaff aStaff = new clsStaff();
+            string TestData = "a staff member name";
+            aStaff.StaffName = TestData;
+            Assert.AreEqual(aStaff.StaffName, TestData);
+        }
+
+      
+        [TestMethod]
+        public void StaffNameMinLessOne()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            string StaffName = "";
+            Error = aStaff.Valid(StaffName, StaffAddress, EmploymentDate, StaffNO);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void StaffNameMaxPlusOne()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            string StaffName = "";
+            StaffName = StaffName.PadLeft(51, '*');
+            Error = aStaff.Valid(StaffName ,StaffAddress, EmploymentDate,StaffNO;
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void StaffNameMaxMinOne()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            string StaffName = "";
+            StaffName = StaffName.PadLeft(49, '*');
+            Error = aStaff.Valid(StaffName, StaffAddress, EmploymentDate,StaffNO);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void StaffNameMinMinOne()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            string StaffName = "";
+            Error = aStaff.Valid(StaffName, StaffAddress, EmploymentDate, StaffNO);
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+        public void StaffAddressOK()
+        {
+            clsStaff aStaff = new clsStaff();
+            string TestData = StaffAddress;
+            aStaff.staffAddress = TestData;
+            Assert.AreEqual(aStaff.staffAddress, TestData, EmploymentDate);
+        }
+
+        [TestMethod]
+        public void StaffAddressMinMinusOne()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            string StaffName = "";
+            Error = aStaff.Valid(StaffName, StaffAddress,EmploymentDate, StaffNO);
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+        public void StaffAddressMaxPlusOne()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            string StaffAddress = "";
+            StaffAddress = StaffAddress.PadLeft(51, '*');
+            Error = aStaff.Valid(StaffName, StaffAddress,EmploymentDate,StaffNO);
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+        public void StaffAddressMin()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            string StaffAddress = "a";
+            Error = aStaff.Valid(StaffName, StaffAddress,EmploymentDate, StaffNO);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void StaffAddressMax()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            string StaffAddress = "";
+            StaffAddress = StaffAddress.PadLeft(50, '*');
+            Error = aStaff.Valid(StaffName, StaffAddress, EmploymentDate, StaffNO);
+            Assert.AreEqual(Error, "");
+        }
+        public void StaffAddressMinPlusOne()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            string StaffName = "aa";
+            Error = aStaff.Valid(StaffName, StaffAddress, EmploymentDate, StaffNO );
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+        public void StaffNoOK()
+        {
+            clsStaff aStaff = new clsStaff();
+            int TestData = 3;
+            aStaff.StaffNO = TestData;
+            Assert.AreEqual(aStaff.StaffNO, TestData);
+        }
+
+        [TestMethod]
+        public void EmploymentDateMinMinusOne()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddYears(-50).AddDays(-1);
+            string EmploymentDate = TestDate.ToString();
+            Error = aStaff.Valid(StaffName, StaffAddress, EmploymentDate, StaffNO);
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+        public void EmploymentDateMinPlusOne()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddYears(-50).AddDays(1);
+            String EmploymentDate = TestDate.ToString();
+            Error = aStaff.Valid(StaffName, StaffAddress, EmploymentDate, StaffNO);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void EmploymentDateMax()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddYears(10);
+            String Employment = TestDate.ToString();
+            Error = aStaff.Valid(StaffName, StaffAddress, EmploymentDate,StaffNO);
+            Assert.AreNotEqual(Error, "");
+        }
+        [TestMethod]
+        public void EmploymentDateExtremeMax()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            DateTime TestDate;
+            TestDate = DateTime.Now.Date;
+            TestDate = TestDate.AddYears(150);
+            String Employment = TestDate.ToString();
+            Error = aStaff.Valid(StaffName, StaffAddress, EmploymentDate, StaffNO);
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+        public void EmploymentDateInvalidDate()
+        {
+            clsStaff aStaff = new clsStaff();
+            string Error = "";
+            String EmploymentDate = "This Date is Invalid!";
+            Error = aStaff.Valid(StaffName, StaffAddress, EmploymentDate,StaffNO);
+            Assert.AreEqual(Error, "");
+        }
+       
+    }
+}
+    
+
