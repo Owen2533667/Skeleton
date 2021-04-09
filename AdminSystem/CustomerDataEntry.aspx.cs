95095068e7a31548441b15f10bf98e4425393869
P2534310 <P2534310@GWHXX653WIN015W>
2021-04-09 16:16:07 +0100

    week 19

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        clsCustomer AnCustomer = new clsCustomer();
+        AnCustomer.CustomerName = txtCustomerName.Text;
+        Session["AnCustomer"] = AnCustomer;
+        Response.Redirect("CustomerViewer.aspx");
     }
 }
